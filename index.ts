2b0a5bfb1f78ea514f388cf8546aa083511732fb
FrankNewII <chunikhin.artem92@gmail.com>
2022-04-27 21:55:59 +0300

    types

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ export * from './posts';
 export * from './users';
 export * from './websocket-messages';
 
-export type Prepend<T, I> = [T, ...I];
-export type Append<T, I> = [...I, T];
+type Prepend<I, T extends unknown[]> = [I, ...T];
+type Append<I, T extends unknown[]> = [...T, I];

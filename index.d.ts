b23b928df70511937f9548baa592bffaa8333600
FrankNewII <chunikhin.artem92@gmail.com>
2022-04-27 23:34:48 +0300

    types

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './posts';
-export * from './users';
-export * from './websocket-messages';
-export declare type Prepend<I, T extends unknown[]> = [I, ...T];
-export declare type Append<I, T extends unknown[]> = [...T, I];
--- posts.js
+++
@@ -1,7 +0,0 @@
-export var ESupportedContentTypes;
-(function (ESupportedContentTypes) {
-    ESupportedContentTypes["Video"] = "video";
-    ESupportedContentTypes["Image"] = "img";
-    ESupportedContentTypes["Text"] = "text";
-    ESupportedContentTypes["Audio"] = "audio";
-})(ESupportedContentTypes || (ESupportedContentTypes = {}));

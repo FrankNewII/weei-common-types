12afa440cb7cced5730842b011380bedb73f884a
FrankNewII <chunikhin.artem92@gmail.com>
2022-08-16 13:57:47 +0300

    comments interfaces

--- src/comments.ts
+++ src/comments.ts
@@ -1,11 +1,12 @@
-import { TWallet } from './users';
+import { IBasicLinkedProfile } from './users';
 
 export interface IBaseComment {
-    text?: string;
-    ownerId: TWallet;
+    text: string;
+    form: IBasicLinkedProfile;
 }
 
 export interface IComment extends IBaseComment {
+    id: number;
     likes: string;
     timestamp: number;
 }
